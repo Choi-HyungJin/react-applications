60fcae88fda9589185f0c6b80294ae50bdb4286a
optimizion <optimizion@gmail.com>
2021-09-16 16:29:11 +0900

    package.json 수정

--- Bin2Dec/webpack.config.js
+++ Bin2Dec/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
